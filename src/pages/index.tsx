67333380c4270aec25f63fc32ed1e901e1ec243a
unknown <marcorezende@outlook.com.br>
2021-02-17 14:12:40 -0300

    remove chackra remaining code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Flex, Heading, Box } from '@chakra-ui/react';
 import Head from 'next/head'
 
 const Home: React.FC = () => {
@@ -8,12 +7,10 @@ const Home: React.FC = () => {
         <title>Create Next App</title>
       </Head>
 
-      <Flex as="main" height="100vh" alignItems="center" justifyContent="center">
-        <Heading size="md" borderWidth="medium" borderRadius="sm" p="2">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Heading>
-      </Flex>
-        
+      <main>
+        <h1>Welcome to <a href="https://nextjs.org">Next.js!</a></h1>
+      </main>
+
     </div>
   )
 }
